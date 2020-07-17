2cf74c6c359fef7acd887fe4a175e2ce4c780c87
abhilash <cabhi3011@gmail.com>
2020-07-17 22:10:52 +0530

    adding changes for event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = { 
-        "count":0,
-     };
-    render() { 
-        return (
-        <React.Fragment>
-            <span className="badge badge-primary m-2">{this.getCount()}</span>
-            <button className="btn btn-secondary btn-sm">Incriment</button>
-            </React.Fragment>
-            );
-        };
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: [],
+  };
 
-        getCount() {
-            return this.state.count === 0 ? "Zero" : this.state.count;
-        }
+  render() {
+    return (
+      <React.Fragment>
+        <span className="badge badge-primary m-2">{this.getCount()}</span>
+        <button className="btn btn-secondary btn-sm" onClick={this.incriment}>
+          Incriment
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  getCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+
+  incriment = () => {
+    console.log("clicked" + this.state.count);
+    this.setState({ count: this.state.count + 1 });
+  };
 }
 
 export default Counter;
