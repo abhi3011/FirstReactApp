685961f0c4c90d05f0c37eed955f9bbe1ac9caa9
abhilash <cabhi3011@gmail.com>
2020-07-18 01:58:29 +0530

    adding parent counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: this.props.value,
     imageUrl: "https://picsum.photos/200",
     tags: [],
   };
 
   render() {
+    console.log("props", this.props);
     return (
-      <React.Fragment>
+      <div>
         <span className="badge badge-primary m-2">{this.getCount()}</span>
-        <button className="btn btn-secondary btn-sm" onClick={this.incriment}>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => this.incriment("product")}
+        >
           Incriment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
@@ -22,8 +26,8 @@ class Counter extends Component {
     return this.state.count === 0 ? "Zero" : this.state.count;
   }
 
-  incriment = () => {
-    console.log("clicked" + this.state.count);
+  incriment = (product) => {
+    console.log("clicked" + product);
     this.setState({ count: this.state.count + 1 });
   };
 }
